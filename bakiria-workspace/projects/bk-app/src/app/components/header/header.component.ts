85376f308a0c8d52296b4ad53986d5da0c5c0e47
fofware <fofware@gmail.com>
2025-04-22 00:28:32 -0300

    Aprendiendo scss y css

--- bakiria-workspace/projects/bk-app/src/app/components/header/header.component.ts
+++ bakiria-workspace/projects/bk-app/src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderLogoComponent } from '../header-logo/header-logo.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [HeaderLogoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
